5eb67c63472360234e197dd31ef6190ee6ad5c03
Pelangi Aji <pelangisokoaji@gmail.com>
2020-06-06 11:09:40 +0700

    Initial commit

---
+++ lcd_dht_rtc_sdcard_alarm_v3.ino
@@ -0,0 +1,182 @@
+const long alarmTime = 10000; //Detik
+int buzzerDuration = 1000; //Mili Detik
+int buzzerSound = 500; //KHz
+long alarm;
+int mill;
+int onTime;
+int trigTime;
+
+int buzzer = 35;
+
+bool setWaktu = false;
+int jam = 16;
+int menit = 42;
+int detik = 0;
+int tanggal = 5;
+int bulan = 6;
+int tahun = 2020;
+
+#include "U8glib.h"
+U8GLIB_ST7920_128X64_1X u8g(8, 9, 10, 11, 4, 5, 6, 7, 1, 2, 3);
+#include <DS3231.h>
+DS3231  rtc(SDA, SCL);
+int pinCS = 53;
+int chk;
+
+#include <DHT.h>;
+DHT dht(31, DHT22);
+DHT dht2(33, DHT22);
+float hum;  
+float hum2;
+float temp;
+float temp2;
+
+char h [5];
+char h2[5];
+char t [5];
+char t2 [5];
+char str1 [20];
+
+#include <SD.h>
+#include <SPI.h>
+File myFile;
+
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here  
+  u8g.setFont(u8g_font_unifont);
+  //u8g.setFont(u8g_font_osb21);
+  u8g.drawStr( 0, 60, "www.SRS-SSMS.com");
+  u8g.drawStr( 32, 15, rtc.getTimeStr());
+  u8g.drawStr( 0, 30, "IN :");
+  u8g.drawStr( 0, 45, "OUT:");
+  u8g.drawStr( 66, 30, "%");
+  u8g.drawStr( 66, 45, "%");
+  u8g.drawStr( 117, 30, "C");
+  u8g.drawStr( 117, 45, "C");
+  u8g.drawStr( 35, 30, (h) );
+  u8g.drawStr( 35, 45, (h2) );
+  u8g.drawStr( 84, 30, (t) );
+  u8g.drawStr( 84, 45, (t2) );
+}
+
+void drawSdFailed(void) {
+  // graphic commands to redraw the complete screen should be placed here  
+  u8g.setFont(u8g_font_unifont);
+  //u8g.setFont(u8g_font_osb21);
+  u8g.drawStr( 0, 10, "Memori tdk Terbaca");
+  u8g.drawStr( 0, 25, "Silahkan reset");
+  delay(50);
+}
+
+void setup(void) {
+  alarm = alarmTime;
+  pinMode(buzzer, OUTPUT);
+  pinMode(pinCS, OUTPUT);
+  rtc.begin();
+  if (setWaktu){
+  rtc.setDate(tanggal, bulan, tahun); //hapus
+  rtc.setTime(jam, menit, detik);    
+  }
+  dht.begin();
+  dht2.begin();
+  // flip screen, if required
+  // u8g.setRot180();
+  
+  // set SPI backup if required
+  //u8g.setHardwareBackup(u8g_backup_avr_spi);
+
+  // assign default color value
+  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
+    u8g.setColorIndex(255);     // white
+  }
+  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
+    u8g.setColorIndex(3);         // max intensity
+  }
+  else if ( u8g.getMode() == U8G_MODE_BW ) {
+    u8g.setColorIndex(1);         // pixel on
+  }
+  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
+    u8g.setHiColorByRGB(255,255,255);
+  }
+
+    // SD Card Initialization
+  if (SD.begin())
+  {
+  } else
+  { 
+      u8g.firstPage();  
+  do {
+    drawSdFailed();
+  } while( u8g.nextPage() );
+    return;
+  }
+}
+
+int timeToInt(void){
+  int hh = (rtc.getHourStr()-0);
+  int h = hh*3600;
+  int mm = (rtc.getMinStr()-0);
+  int m = mm*60;
+  int s = rtc.getSecStr()-0;
+  int convert = h + m + s;
+  return convert;
+}
+
+void loop(void) {
+
+ 
+  hum = dht.readHumidity();
+  hum2 = dht2.readHumidity();
+  temp = dht.readTemperature();
+  temp2 = dht2.readTemperature();
+
+  dtostrf(hum, 3, 1, h);
+  dtostrf(hum2, 3, 1, h2);
+  dtostrf(temp, 3, 1, t);
+  dtostrf(temp2, 3, 1, t2);
+  
+  // picture loop
+  u8g.firstPage();  
+  do {
+    draw();
+  } while( u8g.nextPage() );
+  char st1 = rtc.getTimeStr();
+  char str1[8];
+  strcpy(str1, st1);
+  trigTime = (rtc.getTime().hour + rtc.getTime().min + rtc.getTime().sec);
+  unsigned long currentMillis = millis();
+  if (currentMillis >= alarm){
+      alarm = alarm + alarmTime;
+      myFile = SD.open("hasil.txt", FILE_WRITE);
+      if (myFile) {    
+        myFile.print(rtc.getDateStr());
+        myFile.print(", "); 
+        myFile.print(rtc.getTimeStr());
+        myFile.print(", ");    
+        myFile.print(h);
+        myFile.print(", ");
+        myFile.print(h2);
+        myFile.print(", ");
+        myFile.print(t);
+        myFile.print(", ");
+        myFile.print(t2);
+        myFile.print("");
+        myFile.println();
+        myFile.close(); // close the file
+      }
+      // if the file didn't open, print an error:
+      else {
+        u8g.firstPage();  
+        do {
+          drawSdFailed();
+        } while( u8g.nextPage() );
+      }
+      tone(buzzer, buzzerSound);
+      delay(buzzerDuration);
+      noTone(buzzer);
+  }
+
+  
+  // rebuild the picture after some delay
+  delay(50);
+}
